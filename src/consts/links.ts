5f65ea1365dca31d4a7905722f153c5612a25af7
Den Smalonski <dzianis.smalonski@protofire.io>
2025-08-27 16:27:23 +0200

    refactor: update links to point to Autonomys resources

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,11 +1,11 @@
 export const links = {
-  home: 'https://www.hyperlane.xyz',
-  explorer: 'https://explorer.hyperlane.xyz',
-  discord: 'https://discord.gg/VK9ZUy3aTV',
+  home: 'https://bridge.mainnet.autonomys.xyz/',
+  explorer: 'https://explorer.auto-evm.mainnet.autonomys.xyz/',
+  discord: 'https://autonomys.xyz/discord',
   github: 'https://github.com/hyperlane-xyz/hyperlane-warp-ui-template',
   docs: 'https://docs.hyperlane.xyz',
   gasDocs: 'https://docs.hyperlane.xyz/docs/reference/hooks/interchain-gas',
   chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
-  twitter: 'https://twitter.com/hyperlane',
+  twitter: 'https://twitter.com/AutonomysNet',
   blog: 'https://medium.com/hyperlane',
 };
