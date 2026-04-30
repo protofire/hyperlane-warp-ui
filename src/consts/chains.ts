db0e2582b9df324e41855f43650af4f27f300247
Den <dzianis.smalonski@protofire.io>
2026-04-30 13:24:07 +0200

    feat: add AI3 solana mainnet

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -34,7 +34,7 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
     // SVM chains require mailbox addresses for the token adapters.
     mailbox: solanamainnetAddresses.mailbox,
     rpcUrls: [
-      { http: 'https://api.mainnet-beta.solana.com' },
+      { http: 'https://mainnet.helius-rpc.com/?api-key=e2e542b1-afdb-4f96-8b8f-55977b7a545e' },
       { http: 'https://api.mainnet.solana.com' }
     ],
   },
